9da7864f7e59ae2a672643dba50da72b1611da41
bryanrtboy <bryan@bryanleister.com>
2019-11-24 11:14:08 -0700

    Added Spherical Mapping

--- Assets/Fetch/Scripts/MapPoints.cs
+++ Assets/Fetch/Scripts/MapPoints.cs
@@ -9,6 +9,8 @@ public class MapPoints : FetchNOAAData
     [Tooltip("Game object to create for each point in the dataset")]
     public Transform m_point;
     public bool m_trimZeroHeightWave = false;
+    public bool m_sphericalMapping = false;
+    public float m_sphereRadius = 1;
 
 
     bool m_hasGotText = false;
@@ -51,12 +53,17 @@ public class MapPoints : FetchNOAAData
             if (m_trimZeroHeightWave && ns.waveHeight == 0)
                 continue;
 
-            float x = (m_map.bounds.size.x * ns.longitude) / 360f;
-            float y = (m_map.bounds.size.y * ns.latitude) / 180f;
+            Vector3 mapPosition = Vector3.zero;
+
+            if (m_sphericalMapping)
+                mapPosition = SphericalCoordinate(ns.longitude, ns.latitude, m_sphereRadius);
+            else
+                mapPosition = RectangularCoordinates(ns.longitude, ns.latitude, m_map);
+
 
             if (m_point != null)
             {
-                Transform t = Instantiate(m_point, new Vector3(x + m_map.transform.position.x, y + m_map.transform.position.y, m_map.transform.position.z), Quaternion.identity, this.transform);
+                Transform t = Instantiate(m_point, mapPosition, Quaternion.identity, this.transform);
                 t.GetComponent<Station>().m_stationData = ns;
             }
             else
@@ -64,5 +71,34 @@ public class MapPoints : FetchNOAAData
                 Debug.LogError("Coordinates were created, but no point object to instantiate!");
             }
         }
+    }
+
+    Vector3 RectangularCoordinates(float longi, float lati, MeshRenderer meshRend)
+    {
+
+        float x = (meshRend.bounds.size.x * longi) / 360f;
+        float y = (meshRend.bounds.size.y * lati) / 180f;
+
+        return new Vector3(x + meshRend.transform.position.x, y + meshRend.transform.position.y, meshRend.transform.position.z);
+
+    }
+
+    // Get the spherical Coordinate
+    Vector3 SphericalCoordinate(float longi, float lati, float radius)
+    {
+
+        // Method 1: Transfer to Radians from Degrees
+        // float theta = longi * Mathf.Deg2Rad;
+        // float phi = lati * Mathf.Deg2Rad;
+
+        // float Xpos = radius * Mathf.Cos(theta) * Mathf.Sin(phi);
+        // float Ypos = radius * Mathf.Sin(theta) * Mathf.Sin(phi);
+        // float Zpos = radius * Mathf.Cos(phi);
+
+        // // Set the X,Y,Z pos from the long and lat
+        // return new Vector3(Xpos, Zpos, Ypos);
+
+        return Quaternion.AngleAxis(longi, -Vector3.up) * Quaternion.AngleAxis(lati, -Vector3.right) * new Vector3(0, 0, radius);
+
     }
 }
